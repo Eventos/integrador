c9c3198328fa7fe53351000909a0c1b9ac2c501d
Erico <ericodias1@gmail.com>
2014-06-04 20:39:34 -0300

    colocando arquivo de config no git ignore

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 //DEFINA A PASTA RAÍZ DO SEU PROJETO
-define('SITE_ROOT', '/var/www/integrador/');
+define('SITE_ROOT', '/var/www/projetoIntegrador/');
 
 //DEFINA A URL BASE DO SEU PROJETO
-define('URL_BASE', 'http://localhost/integrador/');
+define('URL_BASE', 'http://localhost/projetoIntegrador/');
 
 //DEFINE O NOME DA PASTA DO SEU PROJETO PARA USO DO FRAMEWORK
-define('FOLDER', '/integrador/');
+define('FOLDER', '/Integrador/');
 
 //DEFINA A CONDIFICAÇÃO QUE VOCÊ USA
 header('Content-Type: text/html; charset=utf-8');
@@ -21,4 +21,4 @@ define('DB_DATABASE', 'integrador');
 //USUÁRIO DO BANCO
 define('DB_USER', 'root');
 //SENHA DO BANCO
-define('DB_PASSWORD', '12345');
+define('DB_PASSWORD', '123456');
