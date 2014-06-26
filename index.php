0d9c7351655acabdf9c72f0d78a8e5f5b5094f35
Andre <andre@andre.(none)>
2014-06-26 17:32:24 -0300

    correcao no acesso a homepage

--- index.php
+++ index.php
@@ -3,5 +3,6 @@ ini_set('display_errors', 1);
 require('core/App.class.php');
 $app = new App();
 $app->ready();
-$router = new RouterAbstract ($_SERVER['REQUEST_URI']);
+
+$router = new RouterAbstract ($_SERVER['REQUEST_URI'].'index');
 
