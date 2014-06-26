3219fcef98d310144813cf9ba538a11059a58c80
Andre <andre@andre.(none)>
2014-06-26 17:55:35 -0300

    voltando erro de homepage

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ require('core/App.class.php');
 $app = new App();
 $app->ready();
 
-$router = new RouterAbstract ($_SERVER['REQUEST_URI'].'index');
+$router = new RouterAbstract ($_SERVER['REQUEST_URI']);
 
